b0ac1c31656da9817236038fd83dc153f7459a87
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-05-22 19:54:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nasport::Application.config.secret_token = '24b597b513e1e874ccf73b32324fd3e2ce8e7ef7472c87bf5cb518130e9ab1752df4954597d23d4b4a20c1b535bccbb70d92b387978f71c98d054fcb6b96b4e7'
