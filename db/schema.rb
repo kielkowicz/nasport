4ba7ce363da19cc97e258eb0d88320dc02feef5d
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-05-22 21:52:54 +0200

    some automagic changes to schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(:version => 20120522193340) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",     :null => false
+    t.string   "encrypted_password",     :default => "",     :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(:version => 20120522193340) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "role"
+    t.string   "role",                   :default => "user"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
