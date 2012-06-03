4cec52a1bcad0a099d153fccdd73f5b80b91ced3
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-06-03 13:03:15 +0200

    Some hack to make heroku working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def join_or_resign_from_event_link(user, event)
-    event.users.include?(user) ? link_to('Zrezygnuj', resign_event_path(event)) : link_to('Dołącz!', join_event_path(event))
+    event.users.include?(user) ? link_to('Zrezygnuj', resign_event_path(event)) : link_to('Dolacz!', join_event_path(event))
   end
 end
