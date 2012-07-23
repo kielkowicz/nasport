8388728b3cecf9df85030a033e1c92f2a70cfee7
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-07-23 14:13:31 +0200

    add layouts, first steps...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def dolacz_do_eventu_link(event)
+  def dolacz_do_eventu_link(event, options={})
     if event.has_free_spot?
-      link_to('Dolacz!', join_event_path(event))
+      link_to('Dolacz!', join_event_path(event), :class=>options[:class])
     else
       "Brak miejsc".html_safe
     end
   end
   
-  def join_or_resign_from_event_link(user, event)
-     event.users.include?(user) ? link_to('Zrezygnuj', resign_event_path(event)) : dolacz_do_eventu_link(event)
+  def join_or_resign_from_event_link(user, event, options={})
+     event.users.include?(user) ? link_to('Zrezygnuj', resign_event_path(event), :class=>options[:class]) : dolacz_do_eventu_link(event, :class=>options[:class])
   end
 end
